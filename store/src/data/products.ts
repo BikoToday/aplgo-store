c4592892cecef060751730bfa282c3db1e4a9ec4
BikoToday <wewin@bikotoday.co.za>
2025-12-31 01:43:03 +0200

    Add product images and update image paths
    Added product images for Daily, Elite, and Premium Collections to the public directory. Updated product image paths in products.ts to use local image files instead of external URLs.

--- store/src/data/products.ts
+++ store/src/data/products.ts
@@ -6,7 +6,7 @@ export const products: Product[] = [
         id: 'nrm',
         code: 'NRM',
         name: 'Normal',
-        image: 'https://aplgo.co.za/Images/products/nrm.png',
+        image: '/Daily Collection/nrm.png',
         whatsappLink: 'https://wa.me/p/25489530677398033/27607586902',
         collection: 'Daily'
     },
@@ -14,7 +14,7 @@ export const products: Product[] = [
         id: 'rlx',
         code: 'RLX',
         name: 'Relax',
-        image: 'https://aplgo.co.za/Images/products/rlx.png',
+        image: '/Daily Collection/rlx.png',
         whatsappLink: 'https://wa.me/p/25805274925737086/27607586902',
         collection: 'Daily'
     },
@@ -22,7 +22,7 @@ export const products: Product[] = [
         id: 'grw',
         code: 'GRW',
         name: 'Grow',
-        image: 'https://aplgo.co.za/Images/products/grw.png',
+        image: '/Daily Collection/grw.png',
         whatsappLink: 'https://wa.me/p/25495067490109759/27607586902',
         collection: 'Daily'
     },
@@ -30,7 +30,7 @@ export const products: Product[] = [
         id: 'sld',
         code: 'SLD',
         name: 'Slide',
-        image: 'https://aplgo.co.za/Images/products/sld.png',
+        image: '/Daily Collection/sld.png',
         whatsappLink: 'https://wa.me/p/25559533077019753/27607586902',
         collection: 'Daily'
     },
@@ -38,7 +38,7 @@ export const products: Product[] = [
         id: 'stp',
         code: 'STP',
         name: 'Stop',
-        image: 'https://aplgo.co.za/Images/products/stp.png',
+        image: '/Elite Collection/stp.png',
         whatsappLink: 'https://wa.me/p/25697660973186173/27607586902',
         collection: 'Daily'
     },
@@ -46,7 +46,7 @@ export const products: Product[] = [
         id: 'gts',
         code: 'GTS',
         name: 'Get Strength',
-        image: 'https://aplgo.co.za/Images/products/gts.png',
+        image: '/Daily Collection/gts.png',
         whatsappLink: 'https://wa.me/p/25346501018343531/27607586902',
         collection: 'Daily'
     },
@@ -54,7 +54,7 @@ export const products: Product[] = [
         id: 'pwr-apricot',
         code: 'PWR APRICOT',
         name: 'Power Apricot',
-        image: 'https://aplgo.co.za/Images/products/pwr-apricot.png',
+        image: '/Daily Collection/pwr-apricot.png',
         whatsappLink: 'https://wa.me/p/25274116472253391/27607586902',
         collection: 'Daily'
     },
@@ -62,7 +62,7 @@ export const products: Product[] = [
         id: 'pwr-lemon',
         code: 'PWR LEMON',
         name: 'Power Lemon',
-        image: 'https://aplgo.co.za/Images/products/pwr-lemon.png',
+        image: '/Daily Collection/pwr-lemon.png',
         whatsappLink: 'https://wa.me/p/25622177277419748/27607586902',
         collection: 'Daily'
     },
@@ -72,7 +72,7 @@ export const products: Product[] = [
         id: 'hrt',
         code: 'HRT',
         name: 'Heart',
-        image: 'https://aplgo.co.za/Images/products/hrt.png',
+        image: '/Premium Collection/hrt.png',
         whatsappLink: 'https://wa.me/p/25622177277419748/27607586902',
         collection: 'Premium'
     },
@@ -80,7 +80,7 @@ export const products: Product[] = [
         id: 'atl',
         code: 'ATL',
         name: 'Alt',
-        image: 'https://aplgo.co.za/Images/products/atl.png',
+        image: '/Premium Collection/alt.png',
         whatsappLink: 'https://wa.me/p/25226350777060693/27607586902',
         collection: 'Premium'
     },
@@ -88,7 +88,7 @@ export const products: Product[] = [
         id: 'mls',
         code: 'MLS',
         name: 'Multi-Spectrum',
-        image: 'https://aplgo.co.za/Images/products/mls.png',
+        image: '/Premium Collection/mls.png',
         whatsappLink: 'https://wa.me/p/25407727472220256/27607586902',
         collection: 'Premium'
     },
@@ -96,7 +96,7 @@ export const products: Product[] = [
         id: 'lft',
         code: 'LFT',
         name: 'Life',
-        image: 'https://aplgo.co.za/Images/products/lft.png',
+        image: '/Premium Collection/lft.png',
         whatsappLink: 'https://wa.me/p/25333431932981448/27607586902',
         collection: 'Premium'
     },
@@ -104,7 +104,7 @@ export const products: Product[] = [
         id: 'hpr',
         code: 'HPR',
         name: 'Hepar',
-        image: 'https://aplgo.co.za/Images/products/hpr.png',
+        image: '/Premium Collection/hpr.png',
         whatsappLink: 'https://wa.me/p/25120277007671174/27607586902',
         collection: 'Premium'
     },
@@ -112,7 +112,7 @@ export const products: Product[] = [
         id: 'ice',
         code: 'ICE',
         name: 'Ice',
-        image: 'https://aplgo.co.za/Images/products/ice.png',
+        image: '/Premium Collection/ice.png',
         whatsappLink: 'https://wa.me/p/32595219546788947/27607586902',
         collection: 'Premium'
     },
@@ -122,7 +122,7 @@ export const products: Product[] = [
         id: 'brn',
         code: 'BRN',
         name: 'Brain',
-        image: 'https://aplgo.co.za/Images/products/brn.png',
+        image: '/Elite Collection/brn.png',
         whatsappLink: 'https://wa.me/p/33289820893965686/27607586902',
         collection: 'Elite'
     },
@@ -130,7 +130,7 @@ export const products: Product[] = [
         id: 'bty',
         code: 'BTY',
         name: 'Beauty',
-        image: 'https://aplgo.co.za/Images/products/bty.png',
+        image: '/Elite Collection/bty.png',
         whatsappLink: 'https://wa.me/p/24645756875098133/27607586902',
         collection: 'Elite'
     },
@@ -138,7 +138,7 @@ export const products: Product[] = [
         id: 'pft',
         code: 'PFT',
         name: 'Perfect',
-        image: 'https://aplgo.co.za/Images/products/pft.png',
+        image: '/Elite Collection/pft.png',
         whatsappLink: 'https://wa.me/p/33294267156838456/27607586902',
         collection: 'Elite'
     },
@@ -146,7 +146,7 @@ export const products: Product[] = [
         id: 'air',
         code: 'AIR',
         name: 'Air',
-        image: 'https://aplgo.co.za/Images/products/air.png',
+        image: '/Elite Collection/air.png',
         whatsappLink: 'https://wa.me/p/25197388923259943/27607586902',
         collection: 'Elite'
     },
@@ -154,7 +154,7 @@ export const products: Product[] = [
         id: 'hpy',
         code: 'HPY',
         name: 'Happy',
-        image: 'https://aplgo.co.za/Images/products/hpy.png',
+        image: '/Elite Collection/hpy.png',
         whatsappLink: 'https://wa.me/p/25510197188620082/27607586902',
         collection: 'Elite'
     }
